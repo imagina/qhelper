88b9309f7672aacf29723e9e02d3bd3fba885294
michael <38920570+msolanoimagina@users.noreply.github.com>
2019-06-14 16:56:19 -0500

    +Update plugin array, method "tree" and "buildTree"

--- _plugins/array.js
+++ _plugins/array.js
@@ -1,59 +1,62 @@
 import {Cookies, LocalStorage} from 'quasar'
 
 class Array {
-  
+
   constructor() {
-  
+
   }
-  
-  /*order array in tree*/
-  tree(elements, parentId = 0) {
+
+  //Order array as tree
+  tree(elements, fields = {label: 'title', id: 'id'}) {
     var trees = []
-    var result = []
-    elements.forEach(i => {
-      i.parentId ? false : i.parentId = 0
+    var response = []
+
+    //Get parents ID
+    elements.forEach(item => {
+      if (!item.parentId) item.parentId = 0//Default parent ID
       let findId = false;
       elements.forEach(j => {
-        if(j.id == i.parentId){
-          findId = j;
-        }
+        if (j.id == item.parentId) findId = j
       })
-      if(!findId){
-        trees[i.parentId] = i.parentId
-      }
+      if (!findId) trees[item.parentId] = item.parentId
     })
-  
-    trees.forEach((tree,index) => {
-      this.builTree(elements,tree).forEach(element => {
-        result.push(element)
+
+    //Recursive funtion to order items
+    let orderItems = (items) => {
+      let responseOrder = []
+      items.forEach(item => {
+        let itemData = {id: item[fields.id], label: item[fields.label]}
+        if (item.children) itemData.children = orderItems(item.children)
+        responseOrder.push(itemData)
       })
-      
+      return responseOrder
+    }
+
+    //Build tree items
+    trees.forEach((tree, index) => {
+      this.builTree(elements, tree).forEach(element => {
+        let itemOrder = {id: element[fields.id], label: element[fields.label]}
+        if(element.children) itemOrder.children = orderItems(element.children)
+        response.push(itemOrder)
+      })
     })
-    
-    return result
+
+    return response//Response
   }
-  
-  builTree(elements, parentId = 0){
+
+  builTree(elements, parentId = 0) {
     var branch = [];
-  
     elements.forEach(element => {
       element.parentId ? false : element.parentId = 0
       if (element.parentId == parentId) {
-      
         var children = this.builTree(elements, element.id);
-      
-        let record = {
-          id: element.id,
-          label: element.title ? element.title : (element.name ? element.name : '-')
-        }
-      
-        if(children.length)
+        let record = element
+        if (children.length)
           record['children'] = children
-      
         branch.push(record);
       }
     })
-  
+
     return branch;
   }
 
@@ -61,10 +64,10 @@ class Array {
    * Order array in object for select
    *
    * @dataArray{object} = [{
-    *   id: type int or string, -> require
-    *   title: type string      -> require or full_name
-    *   full_name: type string  -> require or title
-    * }]
+   *   id: type int or string, -> require
+   *   title: type string      -> require or full_name
+   *   full_name: type string  -> require or title
+   * }]
    *
    */
   select(dataArray, field = 'id') {
@@ -82,29 +85,29 @@ class Array {
 
     return response
   }
-  
+
   /**
    * Return all parents of id in array
    *
    * @dataArray{object} = [{
-    *   id: type int or string, -> require
-    *   title: type string      -> require
-    *   parentId: type string  -> require
-    * }]
+   *   id: type int or string, -> require
+   *   title: type string      -> require
+   *   parentId: type string  -> require
+   * }]
    * @id{int} = 1
    *
    * @Return{object} = {
-    *   name : type String    / 'parent > parent > parent' return all parents and children element
-    *   parents: type String  / 'parent > parent > parent' return only parents
-    *   name: type String     / [1,2,3] return ID parents and children element
-    * }
+   *   name : type String    / 'parent > parent > parent' return all parents and children element
+   *   parents: type String  / 'parent > parent > parent' return only parents
+   *   name: type String     / [1,2,3] return ID parents and children element
+   * }
    *
    */
   parents(dataArray, id) {
     id = parseInt(id)
-    
+
     let response = {name: [], id: []}
-    
+
     if (dataArray) {
       while (id >= 1) {
         let parent = dataArray.find(item => item.id === id)
@@ -115,20 +118,20 @@ class Array {
         }
       }
     }
-    
+
     response.parents = response.name.slice(0, response.name.length - 1).join(' > ')
     response.name = response.name.join(' > ')
-    
+
     return response
   }
-  
+
   /**
    * Return all ID children of id in array
    *
    * @dataArray{object} = [{
-    *   id: type int or string, -> require
-    *   parentId: type string  -> require
-    * }]
+   *   id: type int or string, -> require
+   *   parentId: type string  -> require
+   * }]
    * @id{int} = 1
    *
    * @Return{array} = [1,2,3...] ID of all children
@@ -138,7 +141,7 @@ class Array {
     /*recursive function*/
     let childrens = (parentId) => {
       let response = []
-      
+
       //add to "response" the ID children of parentId
       dataArray.filter((item) => {
         if (item.parentId == parentId) {
@@ -146,44 +149,44 @@ class Array {
             response.push(item.id)
           else
             response.push(item)
-          
+
           let children = childrens(item.id) //get children from this source
-          
-          
+
+
           children.length ? response.push(children) : false
         }
       })
-      
+
       return response
     }
-    
+
     /*return array merge with all children ID*/
     let data = [].concat.apply([], childrens(parseInt(id)))
     !excludeParent ? data.unshift(id) : false //Add ID from parameter
     return data
   }
-  
+
   /* difference between two arrays */
   diff(arr1, arr2) {
     var diff = {};
-    
+
     diff.arr1 = arr1.filter(function (value) {
       if (arr2.indexOf(value) === -1) {
         return value;
       }
     });
-    
+
     diff.arr2 = arr2.filter(function (value) {
       if (arr1.indexOf(value) === -1) {
         return value;
       }
     });
-    
+
     diff.concat = diff.arr1.concat(diff.arr2);
-    
+
     return diff;
   };
-  
+
   /* find into array by param*/
   findByTag(array, tag, value) {
     let response = false
@@ -201,9 +204,9 @@ class Array {
 const array = new Array();
 
 export default ({Vue}) => {
-  
+
   Vue.prototype.$array = array;
-  
+
 }
 
 export {array};
